cb815b9d914f842bba5b57c5e83a6baa013e2723
Ahmad Zaky <humami539@gmail.com>
2023-07-11 06:34:35 +0700

    pull

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,29 +1,29 @@
 <nav class="navbar navbar-expand navbar-light bg-light shadow fixed-top">
-     <!-- Container wrapper -->
-     <div class="container-fluid d-flex justify-content-center">
-     <!-- Left links -->
-     <ul class="navbar-nav ms-auto mb-lg-1 p-1">
-          <li class="nav-item">
-               <a class="nav-link active" aria-current="page" href="{{ url('/') }}">
+    <!-- Container wrapper -->
+    <div class="container-fluid d-flex justify-content-center">
+    <!-- Left links -->
+    <ul class="navbar-nav ms-auto mb-lg-1 p-1">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ url('/') }}">
                     <p>Home</p>
-               </a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="{{ url('/kelompok') }}">
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('/kelompok') }}">
                     <p>Kelompok</p>
-               </a>
-          </li>
-     </ul>
-     <!-- Right links -->
-     <ul class="navbar-nav ms-auto mb-lg-1"> <!-- Add the 'ml-auto' class to align items to the right -->
-          <li class="nav-item">
-               <a class="nav-link">
+            </a>
+        </li>
+    </ul>
+    <!-- Right links -->
+    <ul class="navbar-nav ms-auto mb-lg-1"> <!-- Add the 'ml-auto' class to align items to the right -->
+        <li class="nav-item">
+            <a class="nav-link">
                     <p id="time">0/00/0000 -- 00:00:00 00</p>
-               </a>
-          </li>
-     </ul>
+            </a>
+        </li>
+    </ul>
 </div>
-     <!-- Container wrapper -->
+    <!-- Container wrapper -->
 </nav>
 <script>
     var timeDisplay = document.getElementById("time");
