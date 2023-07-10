d3438641ea27d34d8ad4aa59537bd6060b855fc1
Rai <mraihanzhafran.14@gmail.com>
2023-07-11 06:35:45 +0700

    fixing navbar and footer after git conflic

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,29 +1,30 @@
 <nav class="navbar navbar-expand navbar-light bg-light shadow fixed-top">
-     <!-- Container wrapper -->
-     <div class="container-fluid d-flex justify-content-center">
-     <!-- Left links -->
-     <ul class="navbar-nav ms-auto mb-lg-1 p-1">
-          <li class="nav-item">
-               <a class="nav-link active" aria-current="page" href="{{ url('/') }}">
-                    <p>Home</p>
-               </a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="{{ url('/kelompok') }}">
-                    <p>Kelompok</p>
-               </a>
-          </li>
-     </ul>
-     <!-- Right links -->
-     <ul class="navbar-nav ms-auto mb-lg-1"> <!-- Add the 'ml-auto' class to align items to the right -->
-          <li class="nav-item">
-               <a class="nav-link">
-                    <p id="time">0/00/0000 -- 00:00:00 00</p>
-               </a>
-          </li>
-     </ul>
-</div>
-     <!-- Container wrapper -->
+    <!-- Container wrapper -->
+    <div class="container-fluid d-flex justify-content-center">
+        <!-- Left links -->
+        <ul class="navbar-nav ms-auto mb-lg-1 p-1">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ url('/') }}">
+                    <p class="m-auto">Home</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/kelompok') }}">
+                    <p class="m-auto">Kelompok</p>
+                </a>
+            </li>
+        </ul>
+        <!-- Right links -->
+        <ul class="navbar-nav ms-auto mb-lg-1">
+            <!-- Add the 'ml-auto' class to align items to the right -->
+            <li class="nav-item">
+                <a class="nav-link">
+                    <p id="time" class="m-auto">0/00/0000 -- 00:00:00 00</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- Container wrapper -->
 </nav>
 <script>
     var timeDisplay = document.getElementById("time");
