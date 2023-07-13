362f45956b10bb441594e350db9d4cb59aff1138
Rai <mraihanzhafran.14@gmail.com>
2023-07-13 14:35:41 +0700

    still testing

--- public/assets/js/animation.js
+++
@@ -1,9 +0,0 @@
-anime({
-    targets: ".row .atom_lottie",
-    scale: 2,
-    easing: "easeInOutExpo",
-    direction: "reverse",
-    update: function () {
-        document.querySelector(".title_fisika").style.display = "block";
-    },
-});
--- public/assets/js/lottie.js
+++
@@ -1,6 +0,0 @@
-$(() => {
-    lottie.loadAnimation({
-        container: $("figure.atom_lottie")[0],
-        path: "https://assets2.lottiefiles.com/packages/lf20_gb5bmwlm.json",
-    });
-});
